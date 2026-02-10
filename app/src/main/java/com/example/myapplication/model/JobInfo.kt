b3d09cd76f5fdfca25ec579792a6e224109fe567
alsongard <alsongard@gmail.com>
2026-02-10 22:38:05 +0300

    JobInfo model added and set

--- app/src/main/java/com/example/myapplication/model/JobInfo.kt
+++ app/src/main/java/com/example/myapplication/model/JobInfo.kt
@@ -1,3 +1,18 @@
 package com.example.myapplication.model
 
-data class JobInfo()
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class JobInfo(
+    @StringRes val jobCategory: Int,
+    @StringRes val jobType:Int,
+    @StringRes val jobExperience:Int,
+//    @StringRes val jobLocation:Int?,
+    @StringRes val jobLocation:Int,
+    @StringRes val jobApplicationDeadline: Int,
+    @StringRes val jobCompanyName:Int,
+    @StringRes val jobCompanyWebsite:Int,
+    @StringRes val jobCompanyMission:Int,
+    @StringRes val jobDescription: Int,
+    @DrawableRes val jobCompanyLogo :Int
+)

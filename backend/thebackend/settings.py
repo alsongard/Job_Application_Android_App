4fab08805b5b8929b696006ae1a0ee3397633e77
alsongard <alsongard@gmail.com>
2026-03-21 16:56:06 +0200

    setting files added

--- backend/thebackend/settings.py
+++ backend/thebackend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
+    'user.apps.UserConfig',
+    'jobs.apps.JobsConfig'
 ]
 
 MIDDLEWARE = [
@@ -72,10 +75,20 @@ WSGI_APPLICATION = 'thebackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "my_service",
+            "passfile": "/home/alson-kali/.my_pgpass",
+        },
     }
 }
 

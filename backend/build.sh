6004801935fbd4efdb65dce902afb3be0ca984b9
alsongard <alsongard@gmail.com>
2026-03-22 21:22:46 +0200

    script file for building: update on requirements.txt file pat

--- backend/build.sh
+++ backend/build.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 pip install -r ./backend/requirements.txt
 
-python manage.py collectstatic --no-input
+python ./backend/manage.py collectstatic --no-input
 
 python ./backend/manage.py migrate

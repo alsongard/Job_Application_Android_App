996885e55fe77eaf94283fdb090fe398da9986d3
alsongard <alsongard@gmail.com>
2026-03-22 22:30:20 +0200

    update for hosting

--- backend/thebackend/settings.py
+++ backend/thebackend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-0kr#jqqx6-bnwmd*$53dqtj)wfd516y7xvn=$+9v0&@^#1vvje
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://jobapp-jpcx.onrender.com/', 'localhost']
+ALLOWED_HOSTS = ['jobapp-jpcx.onrender.com/', 'localhost']
 
 
 # Application definition

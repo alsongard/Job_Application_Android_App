e4995ded8fe525dda36b82da385036c03a7691cb
alsongard <alsongard@gmail.com>
2026-03-22 12:57:00 +0200

    finalizing for hosting

--- backend/thebackend/settings.py
+++ backend/thebackend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0kr#jqqx6-bnwmd*$53dqtj)wfd516y7xvn=$+9v0&@^#1vvje'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'false') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.on_render.com', 'localhost']
 
 
 # Application definition

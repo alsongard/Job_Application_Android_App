48425162c1166b44343cf9519f5eef2e5acbde85
alsongard <alsongard@gmail.com>
2026-03-22 11:59:13 +0200

    update to installed_apps for : rest_framework and jobs.app.JobsConfig

--- backend/thebackend/settings.py
+++ backend/thebackend/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.postgres',
     'user.apps.UserConfig',
-    'jobs.apps.JobsConfig'
+    'jobs.apps.JobsConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
